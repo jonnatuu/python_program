a4836ef6accfa9114d6b0ee4364673787b64c537
Jonna O <jonnatuu@icloud.com>
2024-02-15 18:07:34 +0200

    first steps of the application

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('heihei')
+from flask import Flask
+import html_generator as h
+
+app = Flask(__name__) 
+
+@app.route('/') 
+def hello():
+    return h.html_body("sisältö")
---
+++ html_generator.py
@@ -0,0 +1,10 @@
+def html_body(title, body):
+    return f"""
+
+<!doctype html> 
+<html> 
+    <body> 
+        { body }
+    </body> 
+</html>
+"""
